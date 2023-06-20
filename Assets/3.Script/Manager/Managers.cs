3f31b4cd35207c5bf50f7ab1a0e857bbea97a4a5
최영일 <cyi1769@naver.com>
2023-06-20 22:59:28 +0900

    [Create] : GameManager 작성

--- Assets/3.Script/Manager/Managers.cs
+++ Assets/3.Script/Manager/Managers.cs
@@ -9,11 +9,13 @@ public class Managers : MonoBehaviour
     static Managers s_instance; // 유일성이 보장된다
     static Managers Instance { get { Init(); return s_instance; } } // 유일한 매니저를 갖고온다
 
+    GameManager _game =  new GameManager();
     ResourceManager _resource = new ResourceManager();
     SceneManagerEx _scene = new SceneManagerEx();
     EventManager _eventManager = new EventManager();
     PoolManager _pool = new PoolManager();
     UIManager _ui = new UIManager();
+    public static GameManager Manager { get { return Instance._game; } }
     public static ResourceManager Resource { get { return Instance._resource; } }
     public static SceneManagerEx Scene { get { return Instance._scene; } }
     public static EventManager Event { get { return Instance._eventManager; } }
